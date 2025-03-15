0b2e8d0fdf369047d5b4f376268450741c7594d0
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-03-15 09:42:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
@@ -4,37 +4,46 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution {
 
+class Solution {
   public:
-    int minCoins(vector<int> &c, int sum) {
-        sort(c.begin(),c.end());
-        vector<vector<int>> v1(c.size()+1,vector<int>(sum+1,9999));
-        
-        int j=0;
-           for (int i = 0; i <= c.size(); i++) {
-        v1[i][0] = 0;
-    }
+    int minCoins(vector<int> &c, int s) {
+        if(s==0) return 0;
+        vector<vector<int>> v1(c.size()+1,vector<int>(s+1,INT_MAX));
         for(int i=1;i<v1.size();i++){
-            for( j=1;j<=sum;j++){
+            for(int j=1;j<v1[i].size();j++){
                 if(c[i-1]>j){
                     v1[i][j]=v1[i-1][j];
                 }
-                else {
+                else if(c[i-1]==j){
+                    v1[i][j]=1;
+                }
+                else{
+                    int su=v1[i][j-c[i-1]];
+                    if(su==INT_MAX){
+                        
+                    }
+                    else{
+                        su++;
+                    }
+                    int pu=min(su,v1[i-1][j]);
+                    if(pu==INT_MAX){
+                        v1[i][j]=INT_MAX;
+                        
+                        
+                    }
+                    else{
+                        v1[i][j]=pu;
+                    }
+                }
                 
-                    v1[i][j] = min(v1[i - 1][j], v1[i][j - c[i - 1]] + 1);
-               
             }
-                
-                
-               
         }
-        }
+        return v1[c.size()][s]==INT_MAX?-1:v1[c.size()][s];
         
-    
-        return v1[c.size()][sum]==9999?-1:v1[c.size()][sum];
     }
 };
+
 
 //{ Driver Code Starts.
 
