37732819ca693f1480980668cdbe25feffdf865d
balaraju <119840117+Balaraju9@users.noreply.github.com>
2026-04-24 21:02:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.cpp
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.cpp
@@ -1,48 +1,16 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
-   int removeDuplicates(vector<int> &arr) {
-    int n = arr.size(), j = 0;
-    for (int i = 1; i < n; i++) {
-        if (arr[i] != arr[j]) arr[++j] = arr[i];
+    vector<int> removeDuplicates(vector<int> &arr) {
+        // code here
+        vector<int> arr1;
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++){
+            if(mp.find(arr[i])==mp.end()){
+                arr1.push_back(arr[i]);
+            }
+            mp[arr[i]]++;
+            
+        }
+        return arr1;
     }
-    return j + 1;
-}
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        int ans = ob.removeDuplicates(arr);
-        for (int i = 0; i < ans; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
